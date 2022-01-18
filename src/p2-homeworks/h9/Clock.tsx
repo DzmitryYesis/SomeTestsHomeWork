6e5b35f82ede9c89778c52b5d32cf0ebd5f8000e
DzmitryYesis <yesis1988@mail.ru>
2022-01-18 13:37:16 +0300

    HW9. HW9 done without invisible line under time

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,29 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const month = date.getMonth() + 1
+    const stringTime = <span>{date.getHours()}:{date.getMinutes()}:{date.getSeconds()}</span> // fix with date
+    const stringDate = <span>{date.getDate()}.{month < 10 ? '0' + month : month}.{date.getFullYear()}</span>
 
     return (
         <div>

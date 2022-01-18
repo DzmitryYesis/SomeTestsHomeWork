6458163ccce21cbf7f58de3d80a15db31ed04ab0
DzmitryYesis <yesis1988@mail.ru>
2022-01-18 15:14:55 +0300

    HW10. HW10 done!!!!

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,25 @@
 const initState = {
-
+    changeLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+type LoadingActionType = {
+    type: 'CHANGE-LOADING'
+    changeLoading: boolean
+}
+
+type ActionType = LoadingActionType
+
+export const loadingReducer = (state = initState, action: ActionType): typeof initState=> { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'CHANGE-LOADING': {
+            return {...state, changeLoading: action.changeLoading}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean): LoadingActionType => {
+    return {type: 'CHANGE-LOADING', changeLoading:isLoading}
+} // fix any
